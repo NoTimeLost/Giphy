7d5df4aecec6d12734e0974579310038e1cc9303
Vitaly Kozlov <vvit.kozlov@icloud.com>
2023-01-26 16:27:39 +0400

    Initial Commit

---
+++ Giphy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Giphy
+//
+//  Created by Kozlov Vitaly on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
